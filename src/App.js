f195f4eb4fa2a5b67ef2982badd0364dbde1bf5f
Muhamad Bagas Widyatama <bagasmuhamad665@gmail.com>
2023-01-12 00:41:14 +0700

    Translate HomePage HTML to ReactJS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./assets/css/app.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import HomePage from "pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-xl text-red-700">Haloo</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
