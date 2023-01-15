28a8b93c799fca463eabf9ebc70f33db3c8083a9
Muhamad Bagas Widyatama <bagasmuhamad665@gmail.com>
2023-01-15 13:52:43 +0700

    Membuat halaman Product Details dan menambah fitur Add to Cart

--- src/App.js
+++ src/App.js
@@ -6,20 +6,25 @@ import Details from "pages/Details";
 import Cart from "pages/Cart";
 import Success from "pages/Success";
 import NotFound from "pages/NotFound";
+import Provider from "helpers/hooks/useGlobalContext";
 
 function App() {
   return (
-    <div className="App">
+    <Provider>
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<HomePage />}></Route>
-          <Route path="/categories/:idcategory" element={<Details />}></Route>
+          <Route exact path="/categories/:idc" element={<Details />}></Route>
+          <Route
+            path="/categories/:idc/products/:idp"
+            element={<Details />}
+          ></Route>
           <Route path="/cart" element={<Cart />}></Route>
           <Route path="/success" element={<Success />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
-    </div>
+    </Provider>
   );
 }
 
